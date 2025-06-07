2c0364ff59e7943b40513c15c79bdb5e588ddee7
Steven Graziano <draconis36@gmail.com>
2025-06-06 22:55:30 -0700

    working rat zapping, needs tweaks

--- Assets/Scripts/Rat.cs
+++ Assets/Scripts/Rat.cs
@@ -8,9 +8,37 @@ public class Rat : MonoBehaviour
 
     public float speed = 5f;
 
+    SpriteRenderer sr = null;
+
+    void Awake()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
 
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            speed *= -1;
+            sr.flipX = !sr.flipX;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Boundary"))
+        {
+            RatElectrocution zap = GetComponent<RatElectrocution>();
+            if (zap != null)
+                zap.TriggerZappyDeath();
+            else UnityEngine.Debug.LogWarning("RatElectrocution Component missing!");
+        }
+
     }
 }
